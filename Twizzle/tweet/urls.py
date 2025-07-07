4376227464e93f977d59574f53a764875fa33a87
Shishupal Sahu <Shishupalsahu13@gmail.com>
2025-07-07 19:54:20 +0530

    worked on  view for tweet Application and jinja templates and config url

--- Twizzle/tweet/urls.py
+++ Twizzle/tweet/urls.py
@@ -1,7 +1,12 @@
-from . import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.tweet_list, name='tweet_list'),
+    path('create/', views.tweet_create, name='tweet_create'),
+    # path('<int:tweet_id>/edit/', views.tweet_edit, name='tweet_edit'),
+    # path('<int:tweet_id>/delete/', views.tweet_delete, name='tweet_delete'),
+    path('edit/<int:id>/', views.tweet_edit, name='tweet.edit'),
+    path('delete/<int:id>/', views.tweet_delete, name='tweet.delete'),
 ] 
 
